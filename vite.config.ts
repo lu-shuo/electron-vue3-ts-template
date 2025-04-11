c22f1250309f1c779bfa3cc5e57608cf02d789b0
lushuo <lushuo@kedacom.com>
2025-04-11 15:27:20 +0800

    chore: tool chain

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import electron from 'vite-plugin-electron/simple';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
+    vueDevTools(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
@@ -27,4 +31,4 @@ export default defineConfig({
           : {},
     }),
   ],
-})
+});
